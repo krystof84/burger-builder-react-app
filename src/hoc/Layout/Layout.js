120c1fba1efab9d8835df0b2d4b9243ccc6b92f1
Krzysztof <krisczajka@gmail.com>
2018-12-10 19:42:29 +0100

    Add Toolbar and SideDrawer component

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Aux from '../ReactAux/ReactAux';
+import classes from './Layout.css';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    slideDrawerToggleHandler = () => {
+        // this.setState({ showSideDrawer: !this.state.showSideDrawer })
+
+        this.setState(( prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer }
+        });
+    }
+
+    //drawerToglleClicked
+
+    render () {
+        return (    
+        <Aux>
+            <Toolbar drawerToglleClicked={this.slideDrawerToggleHandler}/>
+            <SideDrawer 
+                open={this.state.showSideDrawer} 
+                closed={this.sideDrawerClosedHandler}/>
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </Aux>);
+    }
+} 
+
+export default Layout;
