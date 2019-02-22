a36602793a5b113d7c32f3497abe60e35bba847e
Krzysztof <krisczajka@gmail.com>
2019-02-22 18:33:34 +0100

    Refactoring reducers

---
+++ src/store/actions/index.js
@@ -0,0 +1,12 @@
+export {
+    addIngredient,
+    removeIngredient,
+    initIngredients
+} from './burgerBuilder';
+export {} from './order';
+
+export {
+    purchaseBurger,
+    purchaseInit,
+    fetchOrders
+} from './order';
